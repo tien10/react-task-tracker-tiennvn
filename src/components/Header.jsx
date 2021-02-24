7e48edf0610b9047d77b324d2959c2b89ceeb77e
Tien Nguyen <nguyentien7566@gmail.com>
2021-02-24 23:05:26 +0700

    c

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click');
+  };
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Task Tracker',
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
